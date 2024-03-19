787276f52c08b0cc271d2feb3f9954e6f6e87e2a
DanTheManOg <pagedaniel13@gmail.com>
2024-03-19 23:22:48 +0000

    removed menu bar

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -7,32 +7,10 @@ import { BsTwitterX, BsDiscord  } from "react-icons/bs";
 
 export default function App() {
 
-    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-
-    const menuItems = [
-        { label: "Home", path: "/" },
-        { label: "About", path: "/about" },
-        { label: "Charts", path: "/charts" },
-      ];
-
     return (
-        <Navbar isBordered
-        isMenuOpen={isMenuOpen}
-        onMenuOpenChange={setIsMenuOpen}>
+        <Navbar isBordered>
 
-
-            <NavbarContent className="sm:hidden" justify="start">
-                <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} className="sm:hidden"/>
-            </NavbarContent>
-
-            <NavbarContent className="sm:hidden pr-3" justify="center">
-                <NavbarBrand>
-                    <Image src="/WIFB.png" width={50} height={50} className="m-3" alt="dogwifbat" />
-                    <div className="text-2xl font-bold">dogwifbat</div>
-                </NavbarBrand>
-            </NavbarContent>
-
-            <NavbarContent className="hidden sm:flex gap-4" justify="left">
+            <NavbarContent justify="left">
 
                 <NavbarBrand>
                     <Image src="/WIFB.png" width={50} height={50} className="m-3" alt="dogwifbat" />
@@ -66,24 +44,6 @@ export default function App() {
                 <BsDiscord className="text-white" />
               </Link>
             </NavbarContent>
-
-            <NavbarMenu>
-            {menuItems.map((item, index) => (
-      <NavbarMenuItem key={`${item.label}-${index}`}>
-        <Link href={item.path} passHref> {/* Use Next.js Link and passHref */}
-          <a
-            className="w-full"
-            style={{
-              color: index === 2 ? 'warning' : index === menuItems.length - 1 ? 'danger' : 'foreground', // Adjust the color styling as per your requirement
-            }}
-            size="lg"
-          >
-            {item.label}
-          </a>
-        </Link>
-      </NavbarMenuItem>
-    ))}
-      </NavbarMenu>
 
           </Navbar>
     );
